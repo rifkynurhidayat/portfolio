1e37b5120bd9466c8445657347d153e71386ed85
Rifky Nurhidayat <rifkynurhidayat623@gmail.com>
2026-07-28 14:21:16 +0700

    update data project

--- src/assets/project.js
+++ src/assets/project.js
@@ -1,21 +1,37 @@
 import React from "react";
+ 
+import manage from './project/manage.png';
+import tabulasi from './project/tabulasi.jpeg';
 import marketplace from './project/marketplace.jpg';
-import tokoOnline from './project/tokoOnline.jpg'
+import tokoOnline from './project/tokoOnline.jpg';
 
 const project = [
     {
         id: 1,
-        name: 'Marketplace',
-        logo: marketplace,
-        tech: ['HTML', 'Css', 'laravel', 'Vue js', 'Mysql'],
+        name: 'Manage Office',
+        logo: manage,
+        tech: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'Laravel','AJAX', 'Mysql'],
     },
     {
         id: 2,
-        name: 'Ecommerce',
+        name: 'TabulasiHub',
+        logo: tabulasi,
+        tech: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'Laravel', 'AJAX', 'Mysql'],
+    },
+    {
+        id: 3,
+        name: 'Marketplace',
+        logo: marketplace,
+        tech: ['HTML', 'CSS', 'Bootstrap','Vue.Js','PHP', 'Laravel', 'AJAX', 'Mysql'],
+    },
+    {
+        id: 4,
+        name: 'Toko Online',
         logo: tokoOnline,
-        tech: ['HTML', 'Css', 'laravel', 'Mysql'],
-    }
-];
+        tech: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'Laravel', 'Mysql'],
+    },
+
+    
+]
 
 export default project;
-
