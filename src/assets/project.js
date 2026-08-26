dec0e7159d321bb8b2128ad4fccb8d2a0e638297
Rifky Nurhidayat <rifkynurhidayat623@gmail.com>
2026-08-26 18:57:04 +0700

    update

--- src/assets/project.js
+++ src/assets/project.js
@@ -12,27 +12,31 @@ const project = [
         logo: manage,
         tech: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'Laravel','AJAX', 'Mysql'],
         description: 'Aplikasi ini digunakan untuk mengelola sistem manajemen persuratan di lingkungan internal Ditjen Intram.',
+        link:'https://github.com/rifkynurhidayat/Manage-Office.git',
     },
     {
         id: 2,
         name: 'TabulasiHub',
         logo: tabulasi,
         tech: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'Laravel', 'AJAX', 'Mysql'],
-        description: 'TabulasiHub adalah aplikasi untuk mengelola data kedatangan dan keberangkatan penumpang dari berbagai simpul transportasi, seperti Pelabuhan Merak, Tanjung Priok, Ketapang, dan Bandara Soekarno-Hatta, serta menyajikan perkembangan data dalam bentuk chart secara informatif.',
+        description: 'TabulasiHub merupakan aplikasi untuk mengelola data kedatangan dan keberangkatan penumpang dari berbagai simpul transportasi, seperti Pelabuhan Merak, Tanjung Priok, Ketapang, dan Bandara Soekarno-Hatta, serta menyajikan perkembangan data dalam bentuk chart secara informatif.',
+        link:'https://github.com/rifkynurhidayat/TabulasiHub.git'
     },
     {
         id: 3,
         name: 'Marketplace',
         logo: marketplace,
         tech: ['HTML', 'CSS', 'Bootstrap','Vue.Js','PHP', 'Laravel', 'AJAX','Midtrans', 'Mysql'],
-        description: 'Marketplace adalah aplikasi berbasis web yang memungkinkan pengguna membuka dan mengelola toko secara online, menampilkan produk, serta mengelola transaksi melalui satu platform terintegrasi.',
+        description: 'Dengan adanya Marketplace aplikasi berbasis web ini yang memungkinkan pengguna membuka dan mengelola toko secara online, menampilkan produk, serta mengelola transaksi melalui satu platform terintegrasi.',
+        link:'https://github.com/rifkynurhidayat/Marketplace.git'
     },
     {
         id: 4,
         name: 'Toko Online',
         logo: tokoOnline,
         tech: ['HTML', 'CSS', 'Bootstrap', 'PHP', 'Laravel', 'Mysql','API Midtrans','API RajaOngkir'],
-        description: 'Toko Online adalah aplikasi berbasis web untuk mengelola katalog produk dan penjualan secara online, mulai dari pengelolaan produk hingga proses pemesanan oleh pelanggan',
+        description: 'aplikasi Toko Online dengan berbasis web untuk mengelola katalog produk dan penjualan secara online, mulai dari pengelolaan produk hingga proses pemesanan oleh pelanggan',
+        link:'https://github.com/rifkynurhidayat/Ecommerce-terintegrasi-API-RajaOngkir-dan-Midtrans.git'
     },
 
     
